8e497298e7c451baa8ffceae959235c4cd45e90f
mincheol-jeong <aksemr19@naver.com>
2024-02-29 10:28:29 +0900

    smb 상태 값 변경

--- model/smb.go
+++ model/smb.go
@@ -1,13 +1,15 @@
 package model
 
 type SmbStatus struct {
-	Hostname    string   `json:"hostname"`
-	IpAddress   string   `json:"ip_address"`
-	Port        []string `json:"port"`
 	Names       string   `json:"names"`
 	Description string   `json:"description"`
 	Status      string   `json:"status"`
 	State       string   `json:"state"`
+	Hostname    string   `json:"hostname"`
+	IpAddress   string   `json:"ip_address"`
+	Port        []string `json:"port"`
+	ShareFolder string   `json:"folder_name"`
+	SharePath   string   `json:"path"`
 	Users       Users    `json:"users"`
 } //@name SmbStatus
 type Users struct {

9fd37aa2d9c8fa2f90e69201ae2952d4a06f733a
jeong da-young <bja05008@gmail.com>
2025-02-06 13:05:17 +0900

    수정

--- utils/license/license.go
+++ utils/license/license.go
@@ -2,12 +2,11 @@ package license
 
 import (
 	"Glue-API/utils"
-	"errors"
-
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -24,20 +23,8 @@ type License_type struct {
 
 func License() (output []string, err error) {
 	var stdout []byte
-	//  For Remote
-	//settings, _ := utils.ReadConfFile()
-	//client, err := utils.ConnectSSH(settings.RemoteHostIp, settings.RemoteRootRsaIdPath)
-	//if err != nil {
-	//      err = err
-	//      utils.FancyHandleError(err)
-	//      return
-	//}
-	// //// Defer closing the network connection.
-	//defer client.Close()
-	//// Execute your command.
 
 	// name
-	//cmd, err := client.Command("cat /root/license_test | grep 'name' | awk '{print $3}'")
 	cmd := exec.Command("sh", "-c", "cat /root/license_test | grep 'name' | awk '{print $3}'")
 	stdout, err = cmd.CombinedOutput()
 	if err != nil {
@@ -50,7 +37,6 @@ func License() (output []string, err error) {
 	output = append(output, string(license_info))
 
 	// date
-	// cmd, err = client.Command("cat /root/license_test | grep 'date' | awk '{print $3}'")
 	cmd = exec.Command("sh", "-c", "cat /root/license_test | grep 'date' | awk '{print $3}'")
 	stdout, err = cmd.CombinedOutput()
 	if err != nil {
